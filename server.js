af893c9ee002ea8d63767f916152ae955c0d17ca
hynaga <hynaga3@gmail.com>
2021-07-21 01:22:53 +0900

    Add MySQL Connection Module

--- server.js
+++ server.js
@@ -1,37 +1,30 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const port = 5000;
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+console.log(conf);
+const mysql = require('mysql');
+
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
 
 app.get('/api/customers', function (req, res) {
-    res.send([
-        {
-            id: 1,
-            image: 'https://placeimg.com/64/64/1',
-            name: '홍길동',
-            birthday: '961222',
-            gender: '남자',
-            job: '대학생'
-          },
-          {
-            id: 2,
-            image: 'https://placeimg.com/64/64/2',
-            name: '나호영',
-            birthday: '111222',
-            gender: '여자',
-            job: '주부'
-          },
-          {
-            id: 3,
-            image: 'https://placeimg.com/64/64/3',
-            name: '김현미',
-            birthday: '991222',
-            gender: '남자',
-            job: '회사원'
-          }
-    ])
-})
+    connection.query(
+        "SELECT * from CUSTOMER", (err, rows, fields) => { res.send(rows) }
+    );  
+});
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+app.listen(port, () => console.log(`Listening on port ${port}`));
